0308e0bad7847512c481e09c96de1664d8a0eda7
Guilherme D Patriota <60905310+guipatriota@users.noreply.github.com>
2026-04-13 20:31:18 +0000

    Add initial project structure with POM and application files

---
+++ src/main/java/br/uninter/medalerta/MedAlertaApplication.java
@@ -0,0 +1,12 @@
+package br.uninter.medalerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedAlertaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedAlertaApplication.class, args);
+    }
+}
